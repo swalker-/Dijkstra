3f02fac4905ae16717220669f27ddde18556d55e
Stephen Walker <stephenleewalker@gmail.com>
2015-05-20 18:33:26 -0500

    Removed duplicate code

--- dijkstra.py
+++ dijkstra.py
@@ -18,6 +18,14 @@ def find_shortest_path(graph_tuple):
 
     return calculate_distance(graph, graph_tuple[2], graph_tuple[1], -1, [graph_tuple[1]])
 
+def __valid_point(point, graph, path):
+    if (point.x >= 0) and (point.x < len(graph)):
+        if (point.y >= 0) and (point.y < len(graph[0])):
+            not_repeat_point = not (path.__contains__(point))
+            valid_position = (graph[point.x][point.y] != -1)
+            return not_repeat_point and valid_position
+    return False
+
 def calculate_distance(graph, end_point, current_point, shortest_distance, path):
     distance = graph[current_point.x][current_point.y]
     if current_point == end_point:
@@ -26,9 +34,10 @@ def calculate_distance(graph, end_point, current_point, shortest_distance, path)
         elif distance<shortest_distance:
             shortest_distance = distance
         return (graph, shortest_distance)
-    if current_point.x > 0 and (graph[current_point.x-1][current_point.y] is not -1):
-        new_point = Point(current_point.x-1, current_point.y)
-        if path.__contains__(new_point) is False:
+    points = [Point(current_point.x+1, current_point.y), Point(current_point.x-1, current_point.y),
+              Point(current_point.x, current_point.y+1), Point(current_point.x, current_point.y-1)]
+    for new_point in points:
+        if __valid_point(new_point, graph, path):
             value = graph[new_point.x][new_point.y]
             if (value > distance+1) or (value == 0):
                 graph[new_point.x][new_point.y] = distance+1
@@ -37,37 +46,5 @@ def calculate_distance(graph, end_point, current_point, shortest_distance, path)
                 (g, s) = calculate_distance(graph, end_point, new_point, shortest_distance, new_path)
                 graph = g
                 shortest_distance = s
-    if current_point.x+1 < len(graph) and (graph[current_point.x+1][current_point.y] is not -1):
-        new_point = Point(current_point.x+1, current_point.y)
-        if path.__contains__(new_point) is False:
-            value = graph[new_point.x][new_point.y]
-            if (value > distance+1) or (value == 0):
-                graph[new_point.x][new_point.y] = distance+1
-                new_path = path.copy()
-                new_path.append(new_point)
-                (g, s) = calculate_distance(graph, end_point, new_point, shortest_distance, new_path)
-                graph = g
-                shortest_distance = s
-    if current_point.y > 0 and (graph[current_point.x][current_point.y-1] is not -1):
-        new_point = Point(current_point.x, current_point.y-1)
-        if path.__contains__(new_point) is False:
-            value = graph[new_point.x][new_point.y]
-            if (value > distance+1) or (value == 0):
-                graph[new_point.x][new_point.y] = distance+1
-                new_path = path.copy()
-                new_path.append(new_point)
-                (g, s) = calculate_distance(graph, end_point, new_point, shortest_distance, new_path)
-                graph = g
-                shortest_distance = s
-    if current_point.y+1 < len(graph[0]) and (graph[current_point.x][current_point.y+1] is not -1):
-        new_point = Point(current_point.x, current_point.y+1)
-        if path.__contains__(new_point) is False:
-            value = graph[new_point.x][new_point.y]
-            if (value > distance+1) or (value == 0):
-                graph[new_point.x][new_point.y] = distance+1
-                new_path = path.copy()
-                new_path.append(new_point)
-                (g, s) = calculate_distance(graph, end_point, new_point, shortest_distance, new_path)
-                graph = g
-                shortest_distance = s
+
     return (graph, shortest_distance)
